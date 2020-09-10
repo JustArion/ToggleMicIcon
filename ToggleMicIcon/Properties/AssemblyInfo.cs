5e43ed6908278808077ad854a876aaf4271d9296
Arion-Kun <Arion-Kun@users.noreply.github.com>
2020-09-10 20:29:44 +0200

    Initial Commit

---
+++ ToggleMicIcon/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using ToggleMicIcon;
+using BuildInfo = ToggleMicIcon.BuildInfo;
+
+[assembly: AssemblyTitle(BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(BuildInfo.Company)]
+[assembly: AssemblyProduct(BuildInfo.Name)]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c7f7b303-176a-469c-9c8c-38ccf2ac6f96")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: MelonInfo(typeof(ToggleMicIconClass), BuildInfo.Name, BuildInfo.Version, BuildInfo.Author, BuildInfo.DownloadLink)]
+[assembly: MelonGame("VRChat", "VRChat")]
+
+
---
+++ ToggleMicIcon/ToggleMicIcon.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using MelonLoader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ToggleMicIcon
+{
+    public static class BuildInfo
+    {
+        public const string Author = "arion#1223";
+        public const string Company = null;
+        public const string DownloadLink = "https://github.com/Arion-Kun/ToggleMicIcon/releases";
+        public const string Name = "ToggleMicIcon";
+
+        public const string Version = "1.0.0";
+    }
+    public class ToggleMicIconClass : MelonMod 
+    {
+        //Was going to do some cool volume things but someone wanted it faster so just doing .SetActive XD
+        public static bool ToggleMic;
+        public override void OnApplicationStart()
+        {
+            MelonPrefs.RegisterCategory("ToggleMicIcon", "Toggle Mic Icon");
+            MelonPrefs.RegisterBool("ToggleMicIcon", "DisableMic", false, "Disable Microphone");
+            ToggleMic = MelonPrefs.GetBool("ToggleMicIcon", "DisableMic");
+            MelonLogger.Log("Settings can be configured in UserData\\modprefs.ini or through UIExpansionKit");
+        }
+
+        public override void VRChat_OnUiManagerInit()
+        {
+            ToggleMethod(ToggleMic);
+        }
+        public override void OnModSettingsApplied()
+        {
+            ToggleMic = MelonPrefs.GetBool("ToggleMicIcon", "DisableMic");
+            ToggleMethod(ToggleMic);
+        }
+        public static void ToggleMethod(bool value)
+        {
+            try
+            {
+                GameObject MicIcon = GameObject.Find("/UserInterface/UnscaledUI/HudContent/Hud/VoiceDotParent").gameObject;
+                MicIcon.SetActive(!value);
+            }
+            catch (Exception e)
+            { MelonLogger.LogError("ToggleMethod Error: " + e); }
+
+
+        }
+    }
+}
